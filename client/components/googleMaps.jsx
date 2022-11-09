33d0dfd76b4c04562497c67e8c6d645401b61ff2
Derrick Fleming <derrick.fleming7@gmail.com>
2022-11-09 18:45:12 +0000

    Added googleMaps to search results and adjusted its sizing to make viewable. #11

--- client/components/googleMaps.jsx
+++ client/components/googleMaps.jsx
@@ -1,22 +1,40 @@
 import React from 'react';
-/* import { Loader } from '@googlemaps/js-api-loader';
+import { Loader } from '@googlemaps/js-api-loader';
 
 const loader = new Loader({
   apiKey: process.env.GOOGLE_API,
   version: 'weekly',
   libraries: ['places', 'marker', 'drawing']
 });
-*/
+
 export default class GoogleMaps extends React.Component {
   constructor(props) {
     super(props);
     this.googleMapDiv = React.createRef();
   }
 
+  componentDidMount() {
+    const mapOptions = {
+      center: { lat: 37.0902, lng: -95.7129 },
+      zoom: 4,
+      streetViewControl: false,
+      mapTypeControl: false
+    };
+    loader.load().then(google => {
+      const map = this.map = new window.google.maps.Map(this.googleMapDiv.current, mapOptions);
+      this.setState({
+        google,
+        map
+      });
+      return map;
+    });
+  }
+
   render() {
     return (
       <div
-        className='google-map'
+      id="the-div"
+      className='google-map'
         ref={this.googleMapDiv} />
     );
   }
