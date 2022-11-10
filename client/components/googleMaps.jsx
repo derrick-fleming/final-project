e227fdb84af7c0eb8d8e7dcd5a6442586dc73422
Derrick Fleming <derrick.fleming7@gmail.com>
2022-11-10 00:26:14 +0000

    Refactored code to adjust conditionals and added escape-html package to prevent cross-site scripting attacks. #11

--- client/components/googleMaps.jsx
+++ client/components/googleMaps.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Loader } from '@googlemaps/js-api-loader';
+import escape from 'escape-html';
 
 const loader = new Loader({
   apiKey: process.env.GOOGLE_API,
@@ -44,14 +45,14 @@ export default class GoogleMaps extends React.Component {
         if (states.length !== 0) {
           const bound = new window.google.maps.LatLngBounds();
 
-          states.map(state => {
+          states.forEach(state => {
+            if (state.latitude === '' || state.longititude === '') {
+              return null;
+            }
             const destination = {
               lat: Number(state.latitude),
               lng: Number(state.longitude)
             };
-            if (state.latitude === '' || state.longititude === '') {
-              return null;
-            }
             bound.extend(new window.google.maps.LatLng(destination));
             const marker = new window.google.maps.Marker({
               position: destination,
@@ -59,25 +60,24 @@ export default class GoogleMaps extends React.Component {
             });
             const infoWindow = new window.google.maps.InfoWindow({
               content:
-                '<h6 class="info-title">' + state.name + '</h6>' +
-                '<p class="info-window mb-0">' + state.addresses[0].line1 + '</p>' +
-                '<p class="info-window gold fw-bold">' + state.addresses[0].city + ', ' + state.addresses[0].stateCode + ' ' + state.addresses[0].postalCode + '</p>',
+                '<h6 class="info-title">' + escape(state.name) + '</h6>' +
+                '<p class="info-window mb-0">' + escape(state.addresses[0].line1) + '</p>' +
+                '<p class="info-window gold fw-bold">' + escape(state.addresses[0].city) + escape(', ') + escape(state.addresses[0].stateCode) + escape(' ') + escape(state.addresses[0].postalCode) + '</p>',
               maxWidth: 250,
               ariaLabel: state.name
             });
             marker.addListener('click', () => {
               infoWindow.open(marker.getMap(), marker);
-              map.setZoom(6);
               map.setCenter(marker.getPosition());
+              map.setZoom(marker.position);
             });
             map.addListener('click', () => {
               infoWindow.close();
               map.setZoom(4);
               map.fitBounds(bound);
               map.getCenter();
-              // map.setCenter(map.fitBounds(bound));
             });
-            return marker.setMap(map);
+            marker.setMap(map);
           });
           window.addEventListener('resize', () => {
             map.fitBounds(bound);
