25ba2b3227032721dc69b6dc61112c029d1cda69
Derrick Fleming <derrick.fleming7@gmail.com>
2022-11-10 04:16:24 +0000

    Added filterCards component to home. #2

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import SearchBar from '../components/searchBar';
+import FilterCards from '../components/filterCards';
 
 export default function Home(props) {
   return (
-    <SearchBar />
+    <>
+      <SearchBar />
+      <FilterCards />
+    </>
   );
 }
