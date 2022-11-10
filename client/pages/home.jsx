6e9c95cd5d151f55fac21427fc921e730d4f398a
Derrick Fleming <derrick.fleming7@gmail.com>
2022-11-10 17:48:38 +0000

    Added a modal to the home page when user clicked on browse states or activities. #2

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -5,6 +5,7 @@ import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Modal from 'react-bootstrap/Modal';
 
 /* const activities = ['Astronomy', 'Biking', 'Hiking', 'Camping', 'Birdwatching', 'Museum Exhibits', 'Fishing', 'Scenic Driving', 'Kayaking', 'Boating', 'Guided Tours'];
 const states = [
@@ -64,38 +65,77 @@ const states = [
 ];
 */
 
-export default function Home(props) {
-  return (
-    <>
-      <SearchBar />
-      <Container>
-        <Row className='justify-content-center open-sans'>
-          <Col md={6}>
-            <Card className='mb-4 shadow-sm home-card'>
-              <Card.Img variant="top" alt="Trees illustration" src="/images/placeholder-trees.png" className='image-size' />
-              <Card.Body className='m-2'>
-                <Card.Title className='merriweather fw-bold fs-4'>States</Card.Title>
-                <Card.Text className='fs-6 pb-2'>
-                  Browse through a list of states to discover national parks found within each state.
-                </Card.Text>
-                <Button variant="success" className='merriweather lh-lg'>Select a State</Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col md={6}>
-            <Card className='shadow-sm home-card'>
-              <Card.Img variant="top" alt="Mountain illustration" src="/images/placeholder-yosemite.png" className='image-size' />
-              <Card.Body className='m-2'>
-                <Card.Title className='merriweather fw-bold fs-4'>Activities</Card.Title>
-                <Card.Text className='fs-6 pb-2'>
-                  Browse through a list of activities to explore a park that matches your interests.
-                </Card.Text>
-                <Button variant="success" className='merriweather lh-lg px-4'>Browse</Button>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </Container>;
-    </>
-  );
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false
+    };
+    this.handleClose = this.handleClose.bind(this);
+    this.handleShow = this.handleShow.bind(this);
+  }
+
+  handleClose(event) {
+    this.setState({
+      show: false
+    });
+  }
+
+  handleShow(event) {
+    this.setState({
+      show: true
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <SearchBar />
+        <Container>
+          <Row className='justify-content-center open-sans'>
+            <Col md={6}>
+              <Card className='mb-4 shadow-sm home-card'>
+                <Card.Img variant="top" alt="Trees illustration" src="/images/placeholder-trees.png" className='image-size' />
+                <Card.Body className='m-2'>
+                  <Card.Title className='merriweather fw-bold fs-4'>States</Card.Title>
+                  <Card.Text className='fs-6 pb-2'>
+                    Browse through a list of states to discover national parks found within each state.
+                  </Card.Text>
+                  <Button variant="success" className='merriweather lh-lg' onClick={this.handleShow}>Select a State</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col md={6}>
+              <Card className='mb-4 shadow-sm home-card'>
+                <Card.Img variant="top" alt="Mountain illustration" src="/images/placeholder-yosemite.png" className='image-size' />
+                <Card.Body className='m-2'>
+                  <Card.Title className='merriweather fw-bold fs-4'>Activities</Card.Title>
+                  <Card.Text className='fs-6 pb-2'>
+                    Browse through a list of activities to explore a park that matches your interests.
+                  </Card.Text>
+                  <Button variant="success" className='merriweather lh-lg px-4' onClick={this.handleShow}>Browse</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+
+        <Modal show={this.state.show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={this.handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    );
+  }
+
 }
