ca89abc6421b67de532c635a898af44562fb1d6b
Derrick Fleming <derrick.fleming7@gmail.com>
2022-11-12 00:48:03 +0000

    Added styling to title, description, and accordion colors. #3

--- client/pages/details.jsx
+++ client/pages/details.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Accordion from 'react-bootstrap/Accordion';
+import Col from 'react-bootstrap/Col';
 
 const activities = new Set(
   ['Astronomy',
@@ -55,7 +56,8 @@ export default class ParkDetails extends React.Component {
     const search = this.props.search;
     const parkKey = process.env.PARKS_API;
     const action = 'parkCode=';
-    const link = `https://developer.nps.gov/api/v1/parks?${action}${search}&api_key=${parkKey}`;
+    let link = `https://developer.nps.gov/api/v1/parks?${action}${search}&api_key=${parkKey}`;
+    link = '/get-parks-results.json';
     fetch(link)
       .then(response => response.json())
       .then(states => {
@@ -97,10 +99,10 @@ export default class ParkDetails extends React.Component {
     const address =
       `${park.addresses[0].line1}
       ${park.addresses[0].city}, ${park.addresses[0].stateCode}`;
-    const latitude = park.latitude;
-    const longitude = park.longitude;
-    const apiKey = process.env.GOOGLE_API;
-    const mapLink = `https://maps.googleapis.com/maps/api/staticmap?center=${latitude},${longitude}&markers=|${latitude},${longitude}&zoom=12&size=400x400&key=${apiKey}`;
+    // const latitude = park.latitude;
+    // const longitude = park.longitude;
+    // const apiKey = process.env.GOOGLE_API;
+    // const mapLink = `https://maps.googleapis.com/maps/api/staticmap?center=${latitude},${longitude}&markers=|${latitude},${longitude}&zoom=12&size=400x400&key=${apiKey}`;
     const entranceFees = park.entranceFees.map((fee, index) => {
       return (
         <div key={index}>
@@ -112,60 +114,75 @@ export default class ParkDetails extends React.Component {
     });
     const activityList = this.state.results.activities.filter(activity => activities.has(activity.name));
     return (
-      <Container>
+      <Container p-2>
         <Row>
-          <h3>{name}</h3>
-          <a onClick={this.goBack}>Go Back</a>
+          <Col xs={9}>
+            <h2 className='px-2 merriweather fs-bold'>{name}</h2>
+          </Col>
+          <Col xs={3}>
+            <a className='open-sans go-back text-decoration-none fs-bolder' onClick={this.goBack}>Go Back</a>
+          </Col>
+        </Row>
+        <Row className='justify-content-center'>
+          <Col xs={11}>
+            <img className='shadow-sm p-0 rounded image-details mb-3' src={wikiImage} alt={name} />
+          </Col>
         </Row>
         <Row>
-          <img src={wikiImage} alt={name} />
+          <Col xs={6}>
+            <h3 className='px-2 merriweather fs-bold'> Description </h3>
+          </Col>
         </Row>
         <Row>
-          <p>{description}</p>
+          <Col xs={12}>
+            <p className='p-2 description-text fw-light'>{description}</p>
+          </Col>
         </Row>
-        <Accordion defaultActiveKey="0">
-          <Accordion.Item eventKey="0">
-            <Accordion.Header>
-              <span className='fa-solid fa-person-biking' /> Popular Activities
-            </Accordion.Header>
-            <Accordion.Body>
-              <ol>
-                {
-                  activityList.map(activity => <li key={activity.name}>{activity.name}</li>)
-                }
-              </ol>
-            </Accordion.Body>
-          </Accordion.Item>
-          <Accordion.Item eventKey="1">
-            <Accordion.Header>
-              <span className='fa-solid fa-map-location-dot' /> Address & Directions
-            </Accordion.Header>
-            <Accordion.Body>
-              {address}
-              <br />
-              <img className='static-map' src={mapLink} />
-              <br />
-              {park.directionsInfo}
-            </Accordion.Body>
-          </Accordion.Item>
-          <Accordion.Item eventKey="2">
-            <Accordion.Header>
-              <span className='fa-solid fa-hand-holding-dollar' /> Fees
-            </Accordion.Header>
-            <Accordion.Body>
-              Entrance Fees:
-              {entranceFees}
-            </Accordion.Body>
-          </Accordion.Item>
-          <Accordion.Item eventKey="3">
-            <Accordion.Header>
-              <span className='fa-solid fa-cloud-sun' /> Weather Information
-            </Accordion.Header>
-            <Accordion.Body>
-              {weatherInfo}
-            </Accordion.Body>
-          </Accordion.Item>
-        </Accordion>
+        <Col>
+          <Accordion className='open-sans mb-2' defaultActiveKey="0">
+            <Accordion.Item eventKey="0">
+              <Accordion.Header>
+                <span className='fa-solid fa-person-biking pe-2' /> Popular Activities
+              </Accordion.Header>
+              <Accordion.Body>
+                <ol>
+                  {
+                    activityList.map(activity => <li key={activity.name}>{activity.name}</li>)
+                  }
+                </ol>
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="1">
+              <Accordion.Header>
+                <span className='fa-solid fa-map-location-dot pe-2' /> Address & Directions
+              </Accordion.Header>
+              <Accordion.Body>
+                {address}
+                <br />
+                <img className='static-map' src='images/beach.png' />
+                <br />
+                {park.directionsInfo}
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="2">
+              <Accordion.Header>
+                <span className='fa-solid fa-hand-holding-dollar pe-2' /> Fees
+              </Accordion.Header>
+              <Accordion.Body>
+                Entrance Fees:
+                {entranceFees}
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="3">
+              <Accordion.Header>
+                <span className='fa-solid fa-cloud-sun pe-2' /> Weather Information
+              </Accordion.Header>
+              <Accordion.Body>
+                {weatherInfo}
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+        </Col>
       </Container>
     );
   }
