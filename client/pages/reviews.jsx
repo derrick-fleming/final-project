cd48dc1aaf41cfb12234cce4ee6bba62a1d78e2b
Derrick Fleming <derrick.fleming7@gmail.com>
2022-11-14 00:51:30 +0000

    added park's title to the review page. #4

--- client/pages/reviews.jsx
+++ client/pages/reviews.jsx
@@ -1,9 +1,71 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
 
 export default class ReviewPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      results: [],
+      isLoading: true
+    };
+    this.fetchData = this.fetchData.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    const search = this.props.park;
+    const parkKey = process.env.PARKS_API;
+    const action = 'parkCode=';
+    let link = `https://developer.nps.gov/api/v1/parks?${action}${search}&api_key=${parkKey}`;
+    link = 'get-parks-results.json';
+    fetch(link)
+      .then(response => response.json())
+      .then(states => {
+        const apiEndPoint = 'https://en.wikipedia.org/w/api.php';
+        const imageFetches = states.data.map(state => {
+          const title = state.fullName.replaceAll(' ', '%20');
+          const params = `action=query&format=json&prop=pageimages&titles=${title}&redirects=1&formatversion=2&piprop=thumbnail&pithumbsize=500&pilimit=3`;
+          return (
+            fetch(apiEndPoint + '?' + params + '&origin=*')
+              .then(response => response.json())
+              .then(image => {
+                if (image.query.pages[0].thumbnail === undefined) {
+                  state.wikiImage = '/images/mountains.png';
+                } else {
+                  state.wikiImage = image.query.pages[0].thumbnail.source;
+                }
+              })
+              .catch(err => console.error(err))
+          );
+        });
+        Promise
+          .all(imageFetches)
+          .then(results => {
+            this.setState({
+              results: states.data[0],
+              isLoading: false
+            });
+          });
+      })
+      .catch(err => console.error(err));
+  }
+
   render() {
+    if (this.state.isLoading) {
+      return <div />;
+    }
+    const { name } = this.state.results;
     return (
-      <div />
+      <Container>
+        <Row>
+          <h2>{name}</h2>
+        </Row>
+      </Container>
     );
   }
 }
