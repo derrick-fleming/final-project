a944fff17733697dca99986fe11f112228a70f2f
Derrick Fleming <derrick.fleming7@gmail.com>
2022-11-14 05:24:52 +0000

    Added visitors and acitivities to the form with an event listener included #4

--- client/pages/reviews.jsx
+++ client/pages/reviews.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-// import Row from 'react-bootstrap/Row';
+import Row from 'react-bootstrap/Row';
 import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
+import activities from '../lib/activities';
+
+const visitors = ['Everyone', 'History Buffs', 'Families', 'Casual Travelers', 'Teens & Adults', 'Outdoor Enthusiasts', 'Nature Lovers'];
 
 export default class ReviewPage extends React.Component {
   constructor(props) {
@@ -10,10 +13,15 @@ export default class ReviewPage extends React.Component {
     this.state = {
       results: [],
       isLoading: true,
-      rating: ''
+      rating: '',
+      activities: [],
+      visitors: []
+
     };
     this.fetchData = this.fetchData.bind(this);
     this.handleRating = this.handleRating.bind(this);
+    this.handleCheckBox = this.handleCheckBox.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
   componentDidMount() {
@@ -24,6 +32,33 @@ export default class ReviewPage extends React.Component {
     this.setState({
       rating: event.target.value
     });
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleCheckBox(event) {
+    const target = event.target;
+    const name = event.target.name;
+    const array = this.state[name];
+    if (target.checked === true) {
+
+      this.setState({
+        name: array.push(event.target.id)
+      });
+    } else if (target.checked === false) {
+      const index = array.indexOf(event.target.id);
+      this.setState({
+        name: array.splice(index, 1)
+      });
+    }
   }
 
   fetchData() {
@@ -98,15 +133,75 @@ export default class ReviewPage extends React.Component {
             <h2><span className='fa-regular fa-calendar-days pe-2' />Dates Visited</h2>
             <hr />
             <div>
-              <Form.Label htmlFor='dates'> Start Date: </Form.Label>
-              <input id='dates' type='date'/>
+              <Form.Label htmlFor='start-dates'> Start Date: </Form.Label>
+              <input id='start-dates' type='date'/>
             </div>
             <div>
-              <Form.Label htmlFor='dates'> End Date: </Form.Label>
-              <input id='dates' type='date' />
+              <Form.Label htmlFor='end-dates'> End Date: </Form.Label>
+              <input id='end-dates' type='date' />
             </div>
           </Form.Group>
-
+          <Form.Group>
+            <h2><span className="fa-solid fa-person-biking pe-2" />Recommended Activities</h2>
+            <hr />
+            <Row>
+              {
+                activities.map(activity => {
+                  return (
+                    <Col xs={6} key={activity.name}>
+                      <input type='checkbox' id={activity.name} name='activities' value={activity.name} onChange={this.handleCheckBox}/>
+                      <label htmlFor={activity.name}>{activity.name}</label>
+                    </Col>
+                  );
+                })
+              }
+            </Row>
+          </Form.Group>
+          <Form.Group>
+            <h2><span className="fa-solid fa-person-hiking pe-2" />Recommended Visitors</h2>
+            <hr />
+            <Row>
+              {
+                visitors.map(visitor => {
+                  return (
+                    <Col xs={6} key={visitor}>
+                      <input type='checkbox' id={visitor} name='visitors' value={visitor} onChange={this.handleCheckBox} />
+                      <label htmlFor={visitor}>{visitor}</label>
+                    </Col>
+                  );
+                })
+              }
+            </Row>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label htmlFor='tips'>
+              <span className='fa-solid fa-info-circle' />Tips
+            </Form.Label>
+            <hr />
+            <Form.Text>
+              Provide future visitors with tips for a great experience at Devils Postpile
+            </Form.Text>
+            <Form.Control required id='tips' name='tips' type='text' onChange={this.handleInputChange}/>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label htmlFor='generalThoughts'>
+              <span className='fa-solid fa-lightbulb' />General Thoughts
+            </Form.Label>
+            <hr />
+            <Form.Text>
+              Explain your rating or mention other activites someone should know about this park.            </Form.Text>
+            <Form.Control id='generalThoughts' name='generalThoughts' type='text' onChange={this.handleInputChange} />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label htmlFor='imageUrl'>
+              <span className='fa-solid fa-camera' />Photos
+            </Form.Label>
+            <hr />
+            <Form.Text>
+              Upload your favorite photos from this park.
+            </Form.Text>
+            <Form.Control id='imageUrl' name='imageUrl' type='file' onChange={this.handleInputChange} />
+          </Form.Group>
         </Form>
       </Container>
     );
