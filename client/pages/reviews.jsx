01b356f6f3c09222b7bd0a35be8a490cd18e1969
Derrick Fleming <derrick.fleming7@gmail.com>
2022-11-14 03:32:45 +0000

    Added dates to form. #4

--- client/pages/reviews.jsx
+++ client/pages/reviews.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
+// import Row from 'react-bootstrap/Row';
 import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
 
@@ -9,13 +9,21 @@ export default class ReviewPage extends React.Component {
     super(props);
     this.state = {
       results: [],
-      isLoading: true
+      isLoading: true,
+      rating: ''
     };
     this.fetchData = this.fetchData.bind(this);
+    this.handleRating = this.handleRating.bind(this);
   }
 
   componentDidMount() {
     this.fetchData();
+  }
+
+  handleRating(event) {
+    this.setState({
+      rating: event.target.value
+    });
   }
 
   fetchData() {
@@ -73,20 +81,32 @@ export default class ReviewPage extends React.Component {
             </Col>
             <Col xs={3}>
               <div className='star-radio d-flex flex-row-reverse justify-content-end'>
-                <input required id='rating-5' className='px-1' type='radio' name='rating' value='5' />
+                <input required id='rating-5' className='px-1' type='radio' name='rating' value='5' onClick={this.handleRating} />
                 <label htmlFor="rating-5" className='pt-1 fa-solid fa-star'/>
-                <input className='px-1' id='rating-4' type='radio' name='rating' value='4' />
+                <input className='px-1' id='rating-4' type='radio' name='rating' value='4' onClick={this.handleRating}/>
                 <label htmlFor="rating-4" className='pt-1 fa-solid fa-star d-inline'/>
-                <input className='px-1' id='rating-3' type='radio' name='rating' value='3' />
+                <input className='px-1' id='rating-3' type='radio' name='rating' value='3' onClick={this.handleRating}/>
                 <label htmlFor="rating-3" className='pt-1 fa-solid fa-star'/>
-                <input className='px-1' id='rating-2' type='radio' name='rating' value='2' />
+                <input className='px-1' id='rating-2' type='radio' name='rating' value='2' onClick={this.handleRating}/>
                 <label htmlFor="rating-2" className='pt-1 fa-solid fa-star'/>
-                <input className='px-1' id='rating-1' type='radio' name='rating' value='1' />
+                <input className='px-1' id='rating-1' type='radio' name='rating' value='1' onClick={this.handleRating}/>
                 <label htmlFor="rating-1" className='pt-1 fa-solid fa-star'/>
               </div>
             </Col>
           </Form.Group>
-          <Row />
+          <Form.Group>
+            <h2><span className='fa-regular fa-calendar-days pe-2' />Dates Visited</h2>
+            <hr />
+            <div>
+              <Form.Label htmlFor='dates'> Start Date: </Form.Label>
+              <input id='dates' type='date'/>
+            </div>
+            <div>
+              <Form.Label htmlFor='dates'> End Date: </Form.Label>
+              <input id='dates' type='date' />
+            </div>
+          </Form.Group>
+
         </Form>
       </Container>
     );
