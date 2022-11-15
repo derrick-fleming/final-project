83ab444400c1106314e132d115417ac26aefefd7
Derrick Fleming <derrick.fleming7@gmail.com>
2022-11-15 19:30:04 +0000

    Added fetch to api/accounts to retrieve user's list of states visited. #5

--- client/pages/userAccount.jsx
+++ client/pages/userAccount.jsx
@@ -1,6 +1,22 @@
 import React from 'react';
 
 export default class UserAccount extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      accountId: 1
+    };
+  }
+
+  componentDidMount() {
+    const { accountId } = this.state;
+    fetch(`/api/accounts/${accountId}`)
+      .then(response => response.json())
+      .then(result => {
+      })
+      .catch(err => console.error(err));
+  }
+
   render() {
     return <div />;
   }
