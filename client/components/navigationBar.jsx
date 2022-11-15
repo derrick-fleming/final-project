29e040f1faa0d8f80e7cb14b30391ac61f300a8a
Derrick Fleming <derrick.fleming7@gmail.com>
2022-11-15 18:56:12 +0000

    Updated navigation bar to include link to new infographic page. #5

--- client/components/navigationBar.jsx
+++ client/components/navigationBar.jsx
@@ -75,6 +75,7 @@ export default class NavigationBar extends React.Component {
                     Browse Activities
                   </NavDropdown.Item>
                 </NavDropdown>
+                <Nav.Link className='merriweather fs-6' href="#accounts/user">States Tracker</Nav.Link>
               </Nav>
             </Offcanvas.Body>
           </Navbar.Offcanvas>
