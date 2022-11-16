7c87590c23dcea8d8d695449c6653d1e0a40fdde
Derrick Fleming <derrick.fleming7@gmail.com>
2022-11-16 18:51:12 +0000

    Removed 'not response' from db query #5

--- server/index.js
+++ server/index.js
@@ -46,11 +46,6 @@ app.get('/api/accounts/:accountId', (req, res, next) => {
       const visits = result.rows;
       db.query(total, params)
         .then(response => {
-          if (!response) {
-            res.status(404).json({
-              error: 'Cannot find reviews for account'
-            });
-          }
           const amount = response.rows;
           res.status(200).json([visits, amount]);
         })
