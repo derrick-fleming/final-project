2579cf56b47c1830fae23f0b968fb3a92300a4b1
Derrick Fleming <derrick.fleming7@gmail.com>
2022-11-18 18:55:07 +0000

    Added a componentDidMount to UserReviews and included a fetch with user token. #6

--- client/pages/list-reviews.jsx
+++ client/pages/list-reviews.jsx
@@ -2,6 +2,22 @@ import React from 'react';
 import AppContext from '../lib/app-context';
 
 export default class UserReviews extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    const state = this.props.state;
+    const token = window.localStorage.getItem('park-reviews-jwt');
+    const header = {
+      headers: {
+        'X-Access-Token': token
+      }
+    };
+    fetch(`api/reviews/${state}`, header);
+  }
+
   render() {
     return <div />;
   }
