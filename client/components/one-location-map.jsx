7bc72da004ac1044701778bfc9e49380f1dfb22c
Derrick Fleming <derrick.fleming7@gmail.com>
2023-01-19 23:00:51 +0000

    Refactored google-maps components to use async-await when rendering the different maps.

--- client/components/one-location-map.jsx
+++ client/components/one-location-map.jsx
@@ -25,7 +25,7 @@ export default class SinglePointMap extends React.Component {
     }
   }
 
-  createMap() {
+  async createMap() {
     const park = this.props.results;
     const mapOptions = {
       center: { lat: Number(park.latitude), lng: Number(park.longitude) },
@@ -33,50 +33,49 @@ export default class SinglePointMap extends React.Component {
       streetViewControl: false,
       mapTypeControl: false
     };
-    loader.load().then(google => {
+    try {
+      const google = await loader.load();
       const map = this.map = new window.google.maps.Map(this.googleMapDiv.current, mapOptions);
       this.setState({
         google,
         map
       });
-      return map;
-    })
-      .then(map => {
-        if (park.latitude === '' || park.longitude === '') {
-          return null;
-        }
-        const destination = {
-          lat: Number(park.latitude),
-          lng: Number(park.longitude)
-        };
-        const marker = new window.google.maps.Marker({
-          position: destination,
-          map
-        });
-        const infoWindow = new window.google.maps.InfoWindow({
-          content:
-                '<h6 class="info-title">' + escape(park.name) + '</h6>' +
-                '<p class="info-window mb-0">' + escape(park.addresses[0].line1) + '</p>' +
-                '<p class="info-window gold fw-bold">' + escape(park.addresses[0].city) + escape(', ') + escape(park.addresses[0].stateCode) + escape(' ') + escape(park.addresses[0].postalCode) + '</p>',
-          maxWidth: 250,
-          ariaLabel: park.name
-        });
-        marker.addListener('click', () => {
-          infoWindow.open(marker.getMap(), marker);
-          map.setCenter(marker.getPosition());
-          map.setZoom(10);
-        });
-        map.addListener('click', () => {
-          infoWindow.close();
-          map.setZoom(8);
-        });
-        marker.setMap(map);
-        window.addEventListener('resize', () => {
-          map.getCenter();
-        });
+      if (park.latitude === '' || park.longitude === '') {
+        return null;
+      }
+      const destination = {
+        lat: Number(park.latitude),
+        lng: Number(park.longitude)
+      };
+      const marker = new window.google.maps.Marker({
+        position: destination,
+        map
+      });
+      const infoWindow = new window.google.maps.InfoWindow({
+        content:
+          '<h6 class="info-title">' + escape(park.name) + '</h6>' +
+          '<p class="info-window mb-0">' + escape(park.addresses[0].line1) + '</p>' +
+          '<p class="info-window gold fw-bold">' + escape(park.addresses[0].city) + escape(', ') + escape(park.addresses[0].stateCode) + escape(' ') + escape(park.addresses[0].postalCode) + '</p>',
+        maxWidth: 250,
+        ariaLabel: park.name
+      });
+      marker.addListener('click', () => {
+        infoWindow.open(marker.getMap(), marker);
+        map.setCenter(marker.getPosition());
+        map.setZoom(10);
+      });
+      map.addListener('click', () => {
+        infoWindow.close();
+        map.setZoom(8);
+      });
+      marker.setMap(map);
+      window.addEventListener('resize', () => {
         map.getCenter();
-      })
-      .catch(err => console.error(err));
+      });
+      map.getCenter();
+    } catch (err) {
+      console.error(err);
+    }
   }
 
   render() {
